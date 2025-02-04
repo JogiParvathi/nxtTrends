1ca0a80cc8b271df3d46a35062c84796e15de88a
JogiParvathi <nxtwave@ip-10-2-0-212.ap-south-1.compute.internal>
2025-02-04 12:15:01 +0000

    commit message

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,4 +1,7 @@
-// Write your code here
+import Popup from 'reactjs-popup'
+
+import Payment from '../Payment'
+
 import CartContext from '../../context/CartContext'
 
 import './index.css'
@@ -7,26 +10,32 @@ const CartSummary = () => (
   <CartContext.Consumer>
     {value => {
       const {cartList} = value
-      let total = 0
-      cartList.forEach(eachCartItem => {
-        total += eachCartItem.price * eachCartItem.quantity
-      })
+      const items = cartList.length
+      const totalPrice = cartList.reduce(
+        (acc, item) => acc + item.quantity * item.price,
+        0,
+      )
       return (
-        <>
-          <div className="car-summary-container">
-            <h1 className="heading">
-              <span className="order-total">Order Total:</span>Rs {total} /-
+        <div className="cart-summary-container">
+          <div className="cart-summary-card">
+            <h1 className="cart-items-total-price">
+              Order Total: <span>RS {totalPrice}/-</span>
             </h1>
-            <p className="para">{cartList.length} Items in cart</p>
-            <button type="button" className="checkout-button d-sm-none">
-              Checkout
-            </button>
+            <p className="cart-items-count">{items} Items in cart</p>
 
-            <button type="button" className="checkout-button d-lg-none">
-              Checkout
-            </button>
+            <Popup
+              modal
+              trigger={
+                <button className="checkout-btn" type="button">
+                  Checkout
+                </button>
+              }
+              position="top left"
+            >
+              {close => <Payment close={close} />}
+            </Popup>
           </div>
-        </>
+        </div>
       )
     }}
   </CartContext.Consumer>
